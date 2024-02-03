534b34d096a20ecf8187e660ae004b89c4c8aee0
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-02-03 16:30:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("welcom mr taher")
+console.log("welcom )
 const express = require('express')//obligtoir mil module express
 var bodyParser = require('body-parser');//yrdha json mhma knyt yli jya
 const app = express();//kima hekka express module  le routre
