3ce36ada8c92f726433d9d4df7846defb73fb3dd
habib99052019 <84059797+habib99052019@users.noreply.github.com>
2024-02-03 15:54:32 +0100

    Update condition.js

--- models/condition.js
+++ models/condition.js
@@ -1,16 +1,37 @@
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const  adminSchema  = new mongoose.Schema({
+//    numero:Number,
+//    tiket:{ type: Schema.Types.ObjectId, ref:'ticketSchema'},
+//    solde:Number,
+//    type:String,
+//    coefficient:Number,
+//    gagnion:Boolean,
+//    joueur:{ type: Schema.Types.ObjectId, ref:'joueurSchema'}
+ 
+    
+// });
+// module.exports=mongoose.model('admin',ticketSchema);
+// //aaa
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const  adminSchema  = new mongoose.Schema({
-   numero:Number,
-   tiket:{ type: Schema.Types.ObjectId, ref:'ticketSchema'},
-   solde:Number,
-   type:String,
-   coefficient:Number,
-   gagnion:Boolean,
-   joueur:{ type: Schema.Types.ObjectId, ref:'joueurSchema'}
- 
-    
-});
-module.exports=mongoose.model('admin',ticketSchema);
-//aaa
+const  pubSchema  = new mongoose.Schema({
+   
+    // title:String,
+    // description:String,
+    // images:[],
+    // lien:String,
+    // catigories:String jjjj jjjjjj;;
+     //dynamicData: mongoose.Schema.Types.Mixed
+   name:String,
+   isFacebook:Boolean,
+   isWebSite:Boolean,
+   phone:String,
+   statut:String,
+   isNouveaux:Boolean,
+   email:String,
+   project:String
+  });
+module.exports=mongoose.model('pubSchema',pubSchema);
